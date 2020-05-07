435d52cb62abd5638237ca9381793f5ca847fe4d
tiapnn <tiapnn@yahoo.it>
2020-05-07 13:06:34 +0200

    Added JS Logic

---
+++ main.js
@@ -0,0 +1,141 @@
+function increaserS(oggetto, inc, top) {
+  var currentVal = parseInt($(oggetto).html());
+  if (currentVal < top){
+    if (currentVal < 9) {
+      $(oggetto).html(currentVal*1 + inc);
+      var currentVal = $(oggetto).html();
+      $('#time-left').html('0'+currentVal+":00");
+    } else {
+      $(oggetto).html(currentVal*1 + inc);
+      var currentVal = $(oggetto).html();
+      $('#time-left').html(currentVal+":00");  
+    }
+    
+  }
+  
+}
+
+function decreaserS(oggetto, inc, top) {
+  var currentVal = parseInt($(oggetto).html());
+  if (currentVal > top){
+    if (currentVal <= 10) {
+      $(oggetto).html(currentVal*1 - inc);
+      var currentVal = $(oggetto).html();
+      $('#time-left').html('0'+currentVal+":00");
+    } else {
+      $(oggetto).html(currentVal*1 - inc);
+      var currentVal = $(oggetto).html();
+      $('#time-left').html(currentVal+":00");  
+    }
+  }
+  
+}
+
+
+function increaser(oggetto, inc, top) {
+  var currentVal = parseInt($(oggetto).html());
+  if (currentVal < top){
+    $(oggetto).html(currentVal*1 + inc);
+  }
+  
+}
+
+function decreaser(oggetto, inc, top) {
+  var currentVal = parseInt($(oggetto).html());
+  if (currentVal > top){
+    $(oggetto).html(currentVal*1 - inc);
+  }
+  
+}
+
+function reset() {
+  $('#session-length').html(25);
+  $('#break-length').html(5);
+  $("#time-left").html('25:00');
+  $("#timer-label").html("Pomodoro Time!");
+  clearInterval(counter);
+  clearInterval(breakCounter);
+  $('.hide').show();
+  noRunning = true;
+  running = true
+}
+
+
+var noRunning = true;
+var running = true
+var counter;
+var breakCounter;
+
+function start() {
+  if (noRunning) {
+    noRunning = !noRunning
+    $('.hide').hide();
+    $('#pause').toggle();
+    $('#play').toggle();
+    var count = $('#time-left').html();
+    count = parseInt(count);
+    count = count*60;
+    counter = setInterval(() => {
+      if (running)
+      count = count*1 -1;
+      console.log(count)
+      if (count == 0) {
+        clearInterval(counter);
+        $("#timer-label").html("Break Time!");
+        var beep = document.getElementById("beep");
+        beep.play();
+        var breakCount = $('#break-length').html();
+        breakCount = parseInt(breakCount);
+        breakCount = breakCount*60;
+        breakCounter = setInterval(() => {
+          if (running)
+          breakCount = breakCount*1 -1;
+          if (breakCount == 0) {
+            clearInterval(breakCounter);
+            var beep = document.getElementById("beep");
+            beep.play();
+          }
+          if (breakCount>=600){
+            if (breakCount%60>=10){
+              $("#time-left").html(Math.floor(breakCount/60)+':'+breakCount%60);
+            } else {
+              $("#time-left").html(Math.floor(breakCount/60)+':'+'0'+breakCount%60);
+            }
+          } else {
+            if (breakCount%60>=10){
+              $("#time-left").html('0'+(+Math.floor(breakCount/60)+':'+breakCount%60));
+            } else {
+              $("#time-left").html('0'+(Math.floor(breakCount/60)+':'+'0'+breakCount%60));
+            }
+          }
+        }, 1000); 
+      } 
+      if (count>=600){
+        if (count%60>=10){
+          $("#time-left").html(Math.floor(count/60)+':'+count%60);
+        } else {
+          $("#time-left").html(Math.floor(count/60)+':'+'0'+count%60);
+        }
+      } else {
+        if (count%60>=10){
+          $("#time-left").html('0'+(+Math.floor(count/60)+':'+count%60));
+        } else {
+          $("#time-left").html('0'+(Math.floor(count/60)+':'+'0'+count%60));
+        }
+      }  
+
+        
+    }, 1000);    
+
+  } else {
+    running = !running
+    $('#pause').toggle();
+    $('#play').toggle();
+  }
+  
+}
+
+
+$(document).ready(function () {
+  $('#pause').hide();
+});
